2718ab21c387f7a189f055c73bd28b722d025e80
watfg <113062475+watfg@users.noreply.github.com>
2024-07-14 17:20:23 +0900

    ➕ Added publickey feed

--- src/input/feedurl_list.py
+++ src/input/feedurl_list.py
@@ -1,5 +1,6 @@
 feedurls: list = [
     "https://b.hatena.ne.jp/hotentry/it.rss",
     "https://zenn.dev/feed",
-    "https://qiita.com/popular-items/feed.atom"
+    "https://qiita.com/popular-items/feed.atom",
+    "https://www.publickey1.jp/atom.xml"
 ]
